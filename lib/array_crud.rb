95d51d6d6e00be561b0755038e7401af5ceb2efd
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-08 13:08:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,15 +23,15 @@ array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-array.[2]
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
-array.[0]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-array.[-1]
+array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
